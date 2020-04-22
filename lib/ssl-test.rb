0b079dfe370e07df535680dbc1ed4db391c4bcf4
Fabien Chaynes <fabien.chaynes@ringcentral.com>
2020-04-22 20:34:22 +0200

    Make internal methods private.

--- lib/ssl-test.rb
+++ lib/ssl-test.rb
@@ -45,26 +45,6 @@ module SSLTest
     end
   end
 
-  def self.cert_field_to_hash field
-    field.to_a.each.with_object({}) do |v, h|
-      v = v.to_a
-      h[v[0]] = v[1].encode('UTF-8', undef: :replace, invalid: :replace)
-    end
-  end
-
-  def self.cert_domains cert
-    (Array(cert_field_to_hash(cert.subject)['CN']) +
-      cert_field_to_hash(cert.extensions)['subjectAltName'].split(/\s*,\s*/))
-      .compact
-      .map {|s| s.gsub(/^DNS:/, '') }
-      .uniq
-  end
-
-  def self.matching_domains domains, hostname
-    domains.map {|s| Regexp.new("\A#{Regexp.escape(s).gsub('\*', '[^.]+')}\z") }
-      .select {|domain| domain.match?(hostname) }
-  end
-
   # https://docs.ruby-lang.org/en/2.2.0/OpenSSL/OCSP.html
   # https://stackoverflow.com/questions/16244084/how-to-programmatically-check-if-a-certificate-has-been-revoked#answer-16257470
   # Returns an array with [ocsp_check_failed, certificate_revoked, error_reason, revocation_date]
@@ -129,70 +109,94 @@ module SSLTest
     return [true, nil, e.message, nil]
   end
 
-  def self.follow_ocsp_redirects(uri, data, open_timeout: 5, read_timeout: 5, redirection_limit: 5)
-    return nil if redirection_limit == 0
+  class << self
+    private
 
-    path = uri.path == "" ? "/" : uri.path
-    http = Net::HTTP.new(uri.hostname, uri.port)
-    http.open_timeout = open_timeout
-    http.read_timeout = read_timeout
-
-    http_response = http.post(path, data, "content-type" => "application/ocsp-request")
-    case http_response
-    when Net::HTTPSuccess
-      http_response
-    when Net::HTTPRedirection
-      follow_ocsp_redirects(URI(http_response["location"]), data, open_timeout: open_timeout, read_timeout: read_timeout, redirection_limit: redirection_limit -1)
-    else
-      nil
+    def cert_field_to_hash field
+      field.to_a.each.with_object({}) do |v, h|
+        v = v.to_a
+        h[v[0]] = v[1].encode('UTF-8', undef: :replace, invalid: :replace)
+      end
     end
-  end
 
-  # https://ruby-doc.org/stdlib-2.6.3/libdoc/openssl/rdoc/OpenSSL/OCSP.html#constants-list
-  def self.ocsp_response_status_to_string(response_status)
-    case response_status
-    when OpenSSL::OCSP::RESPONSE_STATUS_INTERNALERROR
-      "Internal error in issuer"
-    when OpenSSL::OCSP::RESPONSE_STATUS_MALFORMEDREQUEST
-      "Illegal confirmation request"
-    when OpenSSL::OCSP::RESPONSE_STATUS_SIGREQUIRED
-      "You must sign the request and resubmit"
-    when OpenSSL::OCSP::RESPONSE_STATUS_TRYLATER
-      "Try again later"
-    when OpenSSL::OCSP::RESPONSE_STATUS_UNAUTHORIZED
-      "Your request is unauthorized"
-    else
-      "Unknown reason"
+    def cert_domains cert
+      (Array(cert_field_to_hash(cert.subject)['CN']) +
+        cert_field_to_hash(cert.extensions)['subjectAltName'].split(/\s*,\s*/))
+        .compact
+        .map {|s| s.gsub(/^DNS:/, '') }
+        .uniq
     end
-  end
 
-  def self.ocsp_soft_fail_return(reason)
-     [false, false, reason, nil].freeze
-  end
+    def matching_domains domains, hostname
+      domains.map {|s| Regexp.new("\A#{Regexp.escape(s).gsub('\*', '[^.]+')}\z") }
+        .select {|domain| domain.match?(hostname) }
+    end
 
-  def self.revocation_reason_to_string(revocation_reason)
-    # https://ruby-doc.org/stdlib-2.4.0/libdoc/openssl/rdoc/OpenSSL/OCSP.html#constants-list
-    case revocation_reason
-    when OpenSSL::OCSP::REVOKED_STATUS_AFFILIATIONCHANGED
-      "The certificate subject's name or other information changed"
-    when OpenSSL::OCSP::REVOKED_STATUS_CACOMPROMISE
-      "This CA certificate was revoked due to a key compromise"
-    when OpenSSL::OCSP::REVOKED_STATUS_CERTIFICATEHOLD
-      "The certificate is on hold"
-    when OpenSSL::OCSP::REVOKED_STATUS_CESSATIONOFOPERATION
-      "The certificate is no longer needed"
-    when OpenSSL::OCSP::REVOKED_STATUS_KEYCOMPROMISE
-      "The certificate was revoked due to a key compromise"
-    when OpenSSL::OCSP::REVOKED_STATUS_NOSTATUS
-      "The certificate was revoked for an unknown reason"
-    when OpenSSL::OCSP::REVOKED_STATUS_REMOVEFROMCRL
-      "The certificate was previously on hold and should now be removed from the CRL"
-    when OpenSSL::OCSP::REVOKED_STATUS_SUPERSEDED
-      "The certificate was superseded by a new certificate"
-    when OpenSSL::OCSP::REVOKED_STATUS_UNSPECIFIED
-      "The certificate was revoked for an unspecified reason"
-    else
-      "Unknown reason"
+    def follow_ocsp_redirects(uri, data, open_timeout: 5, read_timeout: 5, redirection_limit: 5)
+      return nil if redirection_limit == 0
+
+      path = uri.path == "" ? "/" : uri.path
+      http = Net::HTTP.new(uri.hostname, uri.port)
+      http.open_timeout = open_timeout
+      http.read_timeout = read_timeout
+
+      http_response = http.post(path, data, "content-type" => "application/ocsp-request")
+      case http_response
+      when Net::HTTPSuccess
+        http_response
+      when Net::HTTPRedirection
+        follow_ocsp_redirects(URI(http_response["location"]), data, open_timeout: open_timeout, read_timeout: read_timeout, redirection_limit: redirection_limit -1)
+      else
+        nil
+      end
+    end
+
+    # https://ruby-doc.org/stdlib-2.6.3/libdoc/openssl/rdoc/OpenSSL/OCSP.html#constants-list
+    def ocsp_response_status_to_string(response_status)
+      case response_status
+      when OpenSSL::OCSP::RESPONSE_STATUS_INTERNALERROR
+        "Internal error in issuer"
+      when OpenSSL::OCSP::RESPONSE_STATUS_MALFORMEDREQUEST
+        "Illegal confirmation request"
+      when OpenSSL::OCSP::RESPONSE_STATUS_SIGREQUIRED
+        "You must sign the request and resubmit"
+      when OpenSSL::OCSP::RESPONSE_STATUS_TRYLATER
+        "Try again later"
+      when OpenSSL::OCSP::RESPONSE_STATUS_UNAUTHORIZED
+        "Your request is unauthorized"
+      else
+        "Unknown reason"
+      end
+    end
+
+    def ocsp_soft_fail_return(reason)
+       [false, false, reason, nil].freeze
+    end
+
+    def revocation_reason_to_string(revocation_reason)
+      # https://ruby-doc.org/stdlib-2.4.0/libdoc/openssl/rdoc/OpenSSL/OCSP.html#constants-list
+      case revocation_reason
+      when OpenSSL::OCSP::REVOKED_STATUS_AFFILIATIONCHANGED
+        "The certificate subject's name or other information changed"
+      when OpenSSL::OCSP::REVOKED_STATUS_CACOMPROMISE
+        "This CA certificate was revoked due to a key compromise"
+      when OpenSSL::OCSP::REVOKED_STATUS_CERTIFICATEHOLD
+        "The certificate is on hold"
+      when OpenSSL::OCSP::REVOKED_STATUS_CESSATIONOFOPERATION
+        "The certificate is no longer needed"
+      when OpenSSL::OCSP::REVOKED_STATUS_KEYCOMPROMISE
+        "The certificate was revoked due to a key compromise"
+      when OpenSSL::OCSP::REVOKED_STATUS_NOSTATUS
+        "The certificate was revoked for an unknown reason"
+      when OpenSSL::OCSP::REVOKED_STATUS_REMOVEFROMCRL
+        "The certificate was previously on hold and should now be removed from the CRL"
+      when OpenSSL::OCSP::REVOKED_STATUS_SUPERSEDED
+        "The certificate was superseded by a new certificate"
+      when OpenSSL::OCSP::REVOKED_STATUS_UNSPECIFIED
+        "The certificate was revoked for an unspecified reason"
+      else
+        "Unknown reason"
+      end
     end
   end
 end
